c5212ba51b99dd505504a454dc7bbf831feafabe
Diego Quintela <diegolucasq@gmail.com>
2022-02-08 14:05:21 -0500

    get request per id

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({ message: 'working' });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Madrid',
+    description:
+      '«Fui sobre agua edificada, mis muros de fuego son. Esta es mi insignia y blasón»',
+    location: {
+      lat: 40.4168,
+      lng: 3.7038,
+      // address: 'Madrid, Spain', TODO:
+      creator: 'u1',
+    },
+  },
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place });
 });
 
 module.exports = router;
