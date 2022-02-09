7910f09809b328f14b3c8c22241ee8ed5ea4dbbf
Diego Quintela <diegolucasq@gmail.com>
2022-02-09 16:41:56 -0500

    added error model 404

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -24,11 +26,10 @@ router.get('/:pid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error(
-      'Could not find a place for the provided place id.'
+    throw new HttpError(
+      'Could not find a place for the provided place id.',
+      404
     );
-    error.code = 404;
-    return next(error);
   }
 
   res.json({ place });
@@ -42,9 +43,10 @@ router.get('/user/:uid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided user id.');
-    error.code = 404;
-    return next(error);
+    throw new HttpError(
+      'Could not find a place for the provided user id.',
+      404
+    );
   }
   res.json({ place });
 });
